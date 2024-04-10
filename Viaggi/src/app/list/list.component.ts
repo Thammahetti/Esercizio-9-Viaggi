314d8f168a679f3649e2d8470d7eb79dfc3e781d
Thammahetti Thejan <thammahetti.m.t@iisgalvanimi.edu.it>
2024-04-10 07:09:26 +0000

    finito 2

--- Viaggi/src/app/list/list.component.ts
+++ Viaggi/src/app/list/list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  persone = [
+  {destinazione: "Marte",sistemaSolare: "Sistema solare",durataViaggio: 0.5 - 2, spazioportoPartenza: "Terra (SpaceX Starship)",dataPartenza: "2024 (previsto)",dataArrivo: "2025 (previsto)",costoBiglietto: 500000, immagine: "https://it.wikipedia.org/wiki/Marte_%28astronomia%29",}
+  
+ 
+]
 }
